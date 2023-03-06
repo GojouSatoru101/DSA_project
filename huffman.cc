76be707841a5859563cf1e0bb6659e0fc087e1a5
Mejan Lamichhane <077bct047.mejan@pcampus.edu.np>
2023-03-06 23:38:25 +0545

    map works!

--- huffman.cc
+++ huffman.cc
@@ -3,7 +3,7 @@
 #include<unordered_map>
 #define MAX 100 // maximum height of huffman tree
 using namespace std;
-unordered_map<string, int>CodeMap;
+unordered_map<char, string>CodeMap;
 struct Node
 {
     char data;
@@ -26,11 +26,6 @@ Node* create_node(char ch, int freq)
     new_node->data=ch;
     new_node->freq=freq;
     new_node->left=new_node->right=NULL;
-    // if(is_root)
-    // {
-    //     Root=new_node;
-    //     is_root=false;
-    // }
     return new_node;
 }
 
@@ -129,16 +124,16 @@ Node* buildHuffmanTree(char carr[], int freq_arr[], int size)
     return extractmin(heap);
 }
 
-void printArr(int arr[], int n)
+void printArr(int arr[], int n, char key)
 {
 	int i;
-    string value, temp_val;
+    string value;
 	for (i = 0; i < n; ++i)
 	{
         cout << arr[i];
-        temp_val=arr[i];
-        value.append(temp_val);
+        value.append(to_string(arr[i]));
     }
+    CodeMap[key]=value;
 	cout << "\n";
 }
 
@@ -159,9 +154,8 @@ void printCodes(struct Node* root, int arr[],
 
 	// If this is a leaf node, then it contains one of the input characters, print the character and its code from arr[]
 	if (!root->left && !root->right) {
-        CodeMap.emplace(root->data);
 		cout << root->data << ": ";
-        printArr(arr, top);
+        printArr(arr, top, root->data);
 	}
 }
 
@@ -195,4 +189,10 @@ int main()
         }
     }
     Huffman_Code(carr, freq, count);
+    cout<<"\nChecking map!!"<<endl;
+    for (const auto& pair : CodeMap) {
+        cout << pair.first << ": " << pair.second <<endl;
+    // string codeForA = CodeMap['a'];
+    // cout << "Code for 'a': " << codeForA << endl;
+}
 }
